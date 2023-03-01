301d1f23020ec002abb663324db8380b10f44139
Peter van Doorn <pvandoorn@bunq.com>
2023-03-01 13:28:16 +0100

    added www path to allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-%4)0s(84s!p3tb=@%8g7%u&am@yhod3)z%w-iu3)j#xzgwc@$q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mapmaker.vdotvo9a4e2a6.eu-central-1.cs.amazonlightsail.com', '127.0.0.1', 'mapmaker.nl']
-CSRF_TRUSTED_ORIGINS =['mapmaker.vdotvo9a4e2a6.eu-central-1.cs.amazonlightsail.com', '127.0.0.1', 'mapmaker.nl']
+ALLOWED_HOSTS = ['mapmaker.vdotvo9a4e2a6.eu-central-1.cs.amazonlightsail.com', '127.0.0.1', 'mapmaker.nl', 'www.mapmaker.nl']
+CSRF_TRUSTED_ORIGINS =['mapmaker.vdotvo9a4e2a6.eu-central-1.cs.amazonlightsail.com', '127.0.0.1', 'mapmaker.nl', 'www.mapmaker.nl']
 
 
 # Application definition
