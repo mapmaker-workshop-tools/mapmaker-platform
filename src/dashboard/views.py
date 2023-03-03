c4d34af30a71935594611e3d9c980cf72fe8e74c
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-03-03 20:45:51 +0100

    Added fix for not logged in user on prod

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -73,24 +73,27 @@ def calculate_honeycomb(workshop_id):
 
 #@login_required
 def index(request):
-    current_user = request.user #Getting currentuser
-    current_workshop = current_user.active_workshop   #Setting the workshop to a known value from the usersettings. If the user has more workshops they can toggle. 
-    #Get some workshop data for dashboard
-    cardcount = Card.objects.filter(workshop=current_workshop).count()
-    ambitioncount = Card.objects.filter(workshop=current_workshop).filter(cardtype='ambition').count()
-    challengecount = Card.objects.filter(workshop=current_workshop).filter(cardtype='challenge').count()
-    ideacount = Card.objects.filter(workshop=current_workshop).filter(cardtype='idea').count()
-    #Getting all participants for this workshop
-    participants = Workshop.participants.through.objects.filter(workshop=current_workshop)
-    participantcount = participants.count()
-    userIDlist = []
-    current_username = current_user.first_name
-    for i in participants:
-        userIDlist.append(i.customuser_id)
-    participants = CustomUser.objects.filter(pk__in=userIDlist)
-    #Getting the network data so we can draw it
-    networkdata = calculate_network(current_workshop.id)
-    honeycombdata = calculate_honeycomb(current_workshop.id)
-    #Sending context and rendering dashboard
-    context = {"firstname": current_username, "participants":participants,"honeycombdata":honeycombdata, "networkdata": networkdata, "workshop": current_workshop,"ambitioncount":ambitioncount,"ideacount":ideacount, "challengecount":challengecount,  "cardscount":cardcount, "participantcount":participantcount}
-    return render(request, 'dashboard_index.html', context)
+    if request.user.is_authenticated:
+        current_user = request.user #Getting currentuser
+        current_workshop = current_user.active_workshop   #Setting the workshop to a known value from the usersettings. If the user has more workshops they can toggle. 
+        #Get some workshop data for dashboard
+        cardcount = Card.objects.filter(workshop=current_workshop).count()
+        ambitioncount = Card.objects.filter(workshop=current_workshop).filter(cardtype='ambition').count()
+        challengecount = Card.objects.filter(workshop=current_workshop).filter(cardtype='challenge').count()
+        ideacount = Card.objects.filter(workshop=current_workshop).filter(cardtype='idea').count()
+        #Getting all participants for this workshop
+        participants = Workshop.participants.through.objects.filter(workshop=current_workshop)
+        participantcount = participants.count()
+        userIDlist = []
+        current_username = current_user.first_name
+        for i in participants:
+            userIDlist.append(i.customuser_id)
+        participants = CustomUser.objects.filter(pk__in=userIDlist)
+        #Getting the network data so we can draw it
+        networkdata = calculate_network(current_workshop.id)
+        honeycombdata = calculate_honeycomb(current_workshop.id)
+        #Sending context and rendering dashboard
+        context = {"firstname": current_username, "participants":participants,"honeycombdata":honeycombdata, "networkdata": networkdata, "workshop": current_workshop,"ambitioncount":ambitioncount,"ideacount":ideacount, "challengecount":challengecount,  "cardscount":cardcount, "participantcount":participantcount}
+        return render(request, 'dashboard_index.html', context)
+    else:
+        return render(request, 'login.html')
