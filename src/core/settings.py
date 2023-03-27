0f384b457ea10b22dcd6d165b4b6280cd21dc175
Peter van Doorn <pvandoorn@bunq.com>
2023-03-27 16:48:16 +0200

    removed prod stuff

--- src/core/settings.py
+++ src/core/settings.py
@@ -28,13 +28,7 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-#Security Settings
-SECURE_HSTS_SECONDS = True
-SECURE_HSTS_PRELOAD = True
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = env('DEBUG')
