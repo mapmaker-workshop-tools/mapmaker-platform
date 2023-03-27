e3955d0d47c60ab09b86f1b501038a71c2312448
Peter van Doorn <pvandoorn@bunq.com>
2023-03-27 09:53:26 +0200

    Updated workshop settings

--- src/core/utils.py
+++ src/core/utils.py
@@ -19,6 +19,6 @@ def qrgenerator(url, workshop_secret):
   url = str(url)
   buffered = BytesIO()
   img = qrcode.make(url)
-  img.save(buffered, format="PNG")
+  img.save(buffered)
   base64_qrcode = base64.b64encode(buffered.getvalue())
   return str("data:;base64,"+str(base64_qrcode.decode("utf-8")))
