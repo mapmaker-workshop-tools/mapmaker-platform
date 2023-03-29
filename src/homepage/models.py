b7c83e2c7b3eeea6ecd3fe8faabccb2c672148d6
Peter van Doorn <pvandoorn@bunq.com>
2023-03-29 11:00:14 +0200

    added marketing signup list to homepage

--- src/homepage/models.py
+++ src/homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from core.utils import mp
+
+from django.db import models
 
 # Create your models here.
+class EmailList(Model):
+    email = models.EmailField(_("email address"))
+    date_created = models.DateTimeField(default=timezone.now, editable=False)
