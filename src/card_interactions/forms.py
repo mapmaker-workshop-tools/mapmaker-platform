d1a614c532cca7f6d2c76832fcd5405e488792c2
Peter van Doorn <pvandoorn@bunq.com>
2023-03-30 15:06:07 +0200

    removed darkmode

--- src/card_interactions/forms.py
+++ src/card_interactions/forms.py
@@ -17,7 +17,7 @@ tailwind_class = """w-full border-2 border-ch-gray-light
         focus:ring-0 focus:shadow-0"
         """
         
-comment_class = """w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"""
+comment_class = """w-full px-0 text-sm text-gray-900 bg-white border-0  focus:ring-0  """
 
 class CardForm(forms.Form):
     cardtype = forms.ChoiceField(choices = CARD_TYPE_CHOICES, widget=forms.Select(attrs={'class': tailwind_class}))
--- src/homepage/forms.py
+++ src/homepage/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import EmailList
 
 
-tailwind_class = """class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"""
+tailwind_class = """class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5     dark:focus:ring-blue-500 dark:focus:border-blue-500"""
         
-comment_class = """w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"""
+comment_class = """w-full px-0 text-sm text-gray-900 bg-white border-0  focus:ring-0  """
 
 class EmailMarketing(forms.Form):
     email = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': tailwind_class, 'placeholder':'email@email.com', 'label':''}))
