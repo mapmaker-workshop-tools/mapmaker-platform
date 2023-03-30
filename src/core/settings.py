83681b715b522662818e6cd890f9cf9fd07abd24
Peter van Doorn <pvandoorn@bunq.com>
2023-03-30 13:44:55 +0200

    removed browser reload

--- src/core/settings.py
+++ src/core/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
