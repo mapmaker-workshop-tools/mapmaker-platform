5e1c8bd29cb6db3b09d7eab38016ffae3b626de3
Peter van Doorn <pvandoorn@bunq.com>
2023-03-30 13:33:33 +0200

    landingspage

--- src/homepage/forms.py
+++ src/homepage/forms.py
@@ -2,18 +2,12 @@ from django import forms
 from .models import EmailList
 
 
-tailwind_class = """w-full border-2 border-ch-gray-light 
-        bg-ch-gray-dark rounded-lg 
-        focus:outline-ch-green-light focus:outline-0 
-        focus:outline-offset-0 focus:border-2 
-        focus:border-woys-purple focus:shadow-none 
-        focus:ring-0 focus:shadow-0"
-        """
+tailwind_class = """class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"""
         
 comment_class = """w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"""
 
 class EmailMarketing(forms.Form):
-    email = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': tailwind_class}))
+    email = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': tailwind_class, 'placeholder':'email@email.com', 'label':''}))
 
     class Meta:
         model = EmailList
