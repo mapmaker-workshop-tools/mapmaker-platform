1dd6f16100a1aa7a1e1ea5fac586edfc3e9ca9ae
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-04-11 19:26:18 +0200

    revertingto "old" database

--- src/core/settings.py
+++ src/core/settings.py
@@ -111,16 +111,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django_psdb_engine',
-    'NAME': env('DB_NAME'),
-    'HOST': env('DB_HOST'),
-    'PORT': env('DB_PORT'),
-    'USER': env('DB_USER'),
-    'PASSWORD': env('DB_PASSWORD'),
-    'OPTIONS': {'ssl': {'ca': env('MYSQL_ATTR_SSL_CA')}}
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': '3306',
+    }
 }
+
 import sys
 if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
